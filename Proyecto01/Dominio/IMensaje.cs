2ecc3ac015e4955cc17d473c40bc2ae30e2b8cfb
SergioArroyoG <soarroyo-e@udabol.edu.bo>
2022-09-30 10:19:52 -0400

    se agrego a dominio los nuevos proc

--- Proyecto01/Dominio/IMensaje.cs
+++ Proyecto01/Dominio/IMensaje.cs
@@ -1,6 +1,17 @@
-﻿namespace Dominio
+﻿using Modelo;
+using System.Collections.Generic;
+using System;
+
+namespace Dominio
 {
     public interface IMensaje
     {
+        public List<Mensajes> ListarMensajes();
+
+        public List<Mensajes> ListarMensajesUsuario(string userName);
+
+        public bool EliminarMensaje(Int32 idMensaje);
+
+        public bool GuardarMensaje(Mensajes mensaje);
     }
 }
--- Proyecto01/Dominio/Mensaje.cs
+++ Proyecto01/Dominio/Mensaje.cs
@@ -1,7 +1,32 @@
-﻿namespace Dominio
+﻿using Infrestructura;
+using Modelo;
+using System.Collections.Generic;
+using System;
+
+namespace Dominio
 {
     public class Mensaje: IMensaje
     {
+        readonly Coneccion repo = new Coneccion();//verificar readonly
 
+        public List<Mensajes> ListarMensajes()
+        {
+            return repo.ListarMensajes();
+        }
+
+        public List<Mensajes> ListarMensajesUsuario(string userName)
+        {
+            return repo.ListarMensajesUsuario(userName);
+        }
+
+        public bool EliminarMensaje(Int32 idMensaje)
+        {
+            return repo.EliminarMensaje(idMensaje);
+        }
+
+        public bool GuardarMensaje(Mensajes mensaje)
+        {
+            return repo.GuardarMensaje(mensaje);
+        }
     }
 }
