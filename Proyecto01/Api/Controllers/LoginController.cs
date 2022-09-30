754bb7491491b1a574bd5232f636c5d23c587bc8
rog rui <rrogerruiz@gmail.com>
2022-09-30 15:50:52 -0400

    se adiciono Mock de Login

---
+++ Proyecto01/Api/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost]
+        public Boolean Post(String usuario, String password)
+        {
+           
+            return true;
+        }
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
+
