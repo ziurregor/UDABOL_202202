c45aca9ed8a58e4afee4234aeb9d86f57b355a84
rog rui <rrogerruiz@gmail.com>
2022-10-02 19:29:25 -0400

    Postgre integrado falta la implementacion de la base

--- Proyecto01/Infrestructura/ChatPOSTGREContext.cs
+++ Proyecto01/Infrestructura/ChatPOSTGREContext.cs
@@ -3,37 +3,35 @@ using Microsoft.EntityFrameworkCore;
 
 using Modelo;
 
-using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
 
 namespace Infrestructura
 {
-    public class ChatPOSTGREContext : DbContext
-    {
-        protected readonly IConfiguration Configuration;
-        public ChatPOSTGREContext(IConfiguration configuration)
+        public class ChatPOSTGREContext : DbContext
         {
-            Configuration = configuration;
+
+        //public ChatPOSTGREContext(DbContextOptions<ChatPOSTGREContext> options)
+        //    : base(options)
+        //{
+
+        //}
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("User ID =postgres;Password=123456;Server=localhost;Port=5432;Database=UDA;Integrated Security=true;Pooling=true;");
+                //"Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
+        //                              "User ID =postgres;Password=123456;Server=localhost;Port=5432;Database=UDA;Integrated Security=true;Pooling=true;"
+
+        //}
+        public DbSet<Users> Usuarios { get; set; }
+            public DbSet<Roles> Roles { get; set; }
+            public DbSet<Mensajes> Mensajes { get; set; }
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            // connect to postgres with connection string from app settings
-            options.UseNpgsql(Configuration.GetConnectionString("MyWebApiConection"));
-        }
-
-        public DbSet<Users> User { get; set; }
-    ////public DbSet<Roles> Roles { get; set; }
-    ////protected override void OnModelCreating(ModelBuilder modelBuilder)
-    ////{
-
-    ////}
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseNpgsql("ConnectionStrings");
-    //    base.OnConfiguring(optionsBuilder);
-    //}
-}
     
 }
 
---
+++ Proyecto01/Infrestructura/Coneccion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Modelo;
+
+namespace Infrestructura
+{
+    //public class ConexionTexto : IConexion
+    public class Coneccion : IConeccion
+    {
+        ChatSQLiteContext db;
+
+        public bool Conectar(string cadenaDeConexion, Type tipo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Conectar()
+        {
+            db = new ChatSQLiteContext();
+            return true;
+        }
+
+        public bool EliminarRegistro(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EscribirTabla(List<Users> lista)
+        {
+            var itemIndex=0;
+            using (ChatSQLiteContext context = new ChatSQLiteContext())
+            {
+                foreach (var users in lista)
+                {
+                    context.User.Add(users);
+                }
+                context.SaveChanges();
+                 itemIndex = context.User.ToList().Count;// .IndexOf(newTodo);
+
+            }
+            return itemIndex== lista.Count?true:false;
+        }
+
+
+        public bool EscribirTablaMensaje(List<Mensajes> lista)
+        {
+            var itemIndex = 0;
+            using (ChatSQLiteContext context = new ChatSQLiteContext())
+            {
+                foreach (var mensajes in lista)
+                {
+                    context.Mensaje.Add(mensajes);
+                }
+                context.SaveChanges();
+                itemIndex = context.Mensaje.ToList().Count;// .IndexOf(newTodo);
+
+            }
+            return itemIndex == lista.Count ? true : false;
+        }
+
+        public bool Guardar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> LeerTabla()
+        {
+            
+            using (ChatSQLiteContext context = new ChatSQLiteContext())
+            {
+                return context.User.ToList();
+                //throw new NotImplementedException();
+
+
+
+            }
+            
+        }
+
+        public bool OptieneUsuario(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
