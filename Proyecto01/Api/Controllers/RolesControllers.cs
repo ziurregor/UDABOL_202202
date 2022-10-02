572333c9403addf9e0a5b54767a6d911f3febf27
rog rui <rrogerruiz@gmail.com>
2022-10-02 19:45:13 -0400

    Se unio a main postgre y Api

---
+++ Proyecto01/Api/Controllers/RolesControllers.cs
@@ -0,0 +1,57 @@
+﻿using EntityPosgardo.infraestructura;
+using EntityPosgardo.modelo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityPosgardo.Controllers
+{
+    [ApiController]
+    [Route("api/roles")]
+    public class RolesControllers : ControllerBase
+    {
+        private readonly InfraestructuraDbContext context;
+
+        public RolesControllers(InfraestructuraDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("get")]
+        public async Task<IEnumerable<Roles>> Get()
+        {
+            return await context.Roles.ToListAsync();
+        }
+
+        [HttpPost("post")]
+        public async Task<ActionResult> Post(Roles roles)
+        {
+            context.Add(roles);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> update(Roles request)
+        {
+            var rol = await context.Roles.FirstOrDefaultAsync(r => r.RolId== request.RolId);
+
+            rol.Nombre= request.Nombre;
+            context.Update(rol);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        public async Task<ActionResult> delete(int id)
+        {
+            var roles = await context.Roles.FirstOrDefaultAsync(r => r.RolId == id);
+            if (roles is null)
+            {
+                return NotFound();
+            }
+            context.Remove(roles);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Proyecto01/Api/Controllers/UsuariosControllers .cs
@@ -0,0 +1,63 @@
+﻿using EntityPosgardo.infraestructura;
+using EntityPosgardo.modelo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityPosgardo.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    public class UsuariosControllers : ControllerBase
+    {
+        private readonly InfraestructuraDbContext context;
+
+        public UsuariosControllers(InfraestructuraDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("get")]
+        public async Task<IEnumerable<Usuarios>> Get()
+        {
+            return await context.Usuarios.ToListAsync();
+        }
+
+        [HttpPost("post")]
+        public async Task<ActionResult> Post(Usuarios user)
+        {
+            context.Add(user);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> update(Usuarios request)
+        {
+            var user = await context.Usuarios.FirstOrDefaultAsync(r => r.UserId == request.UserId);
+
+            if (user == null)
+                return BadRequest("Usuario no encontrado");
+            
+            user.Usuario = request.Usuario;
+            user.Name = request.Name;
+            user.Edad= request.Edad;
+            user.Contraseña = request.Contraseña;
+            context.Update(user);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        public async Task<ActionResult> delete(int id)
+        {
+            var usuarios = await context.Usuarios.FirstOrDefaultAsync(r => r.UserId == id);
+            if (usuarios is null)
+            {
+                return NotFound();
+            }
+            context.Remove(usuarios);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
